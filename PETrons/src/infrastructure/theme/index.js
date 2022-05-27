fd1c88bd82587c5fcf02ee4d78591ab122854a54
nicoleleow <e0774639@u.nus.edu>
2022-05-27 17:33:48 +0800

    set up the themes

---
+++ PETrons/src/infrastructure/theme/index.js
@@ -0,0 +1,13 @@
+import { space, lineHeights } from "./spacing";
+import { sizes } from "./sizes";
+import { fonts, fontWeights, fontSizes } from "./fonts";
+
+export const theme = {
+  colors,
+  space,
+  lineHeights,
+  sizes,
+  fonts,
+  fontSizes,
+  fontWeights,
+};
