0bb17b7f05da8fe734f0abd6c88a9c7a0b1e353e
nicoleleow <e0774639@u.nus.edu>
2022-05-28 12:13:27 +0800

    set up and linked to firebase

---
+++ PETrons/firebase/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBN-1QrWO7woeGf7irPB_wnHt587R5HBXQ",
+  authDomain: "petrons-39ae6.firebaseapp.com",
+  projectId: "petrons-39ae6",
+  storageBucket: "petrons-39ae6.appspot.com",
+  messagingSenderId: "806740991321",
+  appId: "1:806740991321:web:2e50c56826e07cef8b0048",
+};
+
+const app = initializeApp(firebaseConfig);
+export const authentication = getAuth(app);
