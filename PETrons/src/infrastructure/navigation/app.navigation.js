2e4fd9c5fee71c34dfcdb5cfcfcc2f17d5ed0162
nicoleleow <e0774639@u.nus.edu>
2022-05-29 19:47:35 +0800

    added icons to the bottom navigation bar in homepage

--- PETrons/src/infrastructure/navigation/app.navigation.js
+++ PETrons/src/infrastructure/navigation/app.navigation.js
@@ -2,6 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet, SafeAreaView, Text, View, Platform } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 import { Mainpage } from "../../features/mainpage/mainpage";
 import { SafeArea } from "../../components/utility/safe-area.component";
@@ -17,10 +19,41 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={Mainpage} />
-      <Tab.Screen name="Activity" component={Activity} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "md-home";
+          } else if (route.name === "Activity") {
+            iconName = "md-notifications";
+          } else if (route.name === "Profile") {
+            iconName = "md-people";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen
+        options={{ headerShown: false }}
+        name="Home"
+        component={Mainpage}
+      />
+      <Tab.Screen
+        options={{ headerShown: false }}
+        name="Activity"
+        component={Activity}
+      />
+      <Tab.Screen
+        options={{ headerShown: false }}
+        name="Profile"
+        component={Profile}
+      />
     </Tab.Navigator>
   );
 }
