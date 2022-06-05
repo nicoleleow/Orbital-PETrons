b6e9efe45a0c3684743836090717a30fed2836ea
tungkei <e0774490@u.nus.edu>
2022-06-05 22:39:57 +0800

    add searchbar to share stories page

--- PETrons/src/features/mainpage/share-stories.js
+++ PETrons/src/features/mainpage/share-stories.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, Text, View, StyleSheet } from "react-native";
 import styled from "styled-components/native";
+import { Searchbar } from 'react-native-paper';
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
@@ -15,10 +16,41 @@ const StoriesPageHeader = styled(Text)`
   font-family: ${(props) => props.theme.fonts.body};
 `;
 
-export const StoriesPage = () => (
-  <SafeArea>
-    <View>
-      <StoriesPageHeader>Share Stories</StoriesPageHeader>
-    </View>
-  </SafeArea>
-);
+export const StoriesPage = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = query => setSearchQuery(query);
+
+  // need to create availablePets.js to get list of available pets and their details (ie database of pets)
+  const filteredPets = availablePets.filter(pet => {
+    return pet.name.toLowerCase().includes(searchQuery.toLowerCase());
+  })
+
+  return (
+    <SafeArea>
+
+      <View>
+        <StoriesPageHeader>Share Stories</StoriesPageHeader>
+      </View>
+
+      <View>
+        <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        style={styles.search}
+        />
+      </View>
+      
+      
+    </SafeArea>
+  )
+};
+
+
+const styles = StyleSheet.create({
+  search: {
+    margin:40,
+    backgroundColor: '#D3D3D3',
+  }
+})
