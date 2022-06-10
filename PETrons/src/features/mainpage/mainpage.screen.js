6e240e0da3380f64db17bdd9799906222bc24cb2
nicoleleow <e0774639@u.nus.edu>
2022-06-10 13:58:28 +0800

    uploading images from camera or camera roll

--- PETrons/src/features/mainpage/mainpage.screen.js
+++ PETrons/src/features/mainpage/mainpage.screen.js
@@ -1,6 +1,127 @@
+// import React from "react";
+// import {
+//   SafeAreaView,
+//   View,
+//   Dimensions,
+//   TouchableOpacity,
+//   Image,
+// } from "react-native";
+// import { Menu } from "react-native-paper";
+// import styled from "styled-components/native";
+// import { Text } from "../../components/typography/text.component";
+
+// const { width } = Dimensions.get("screen");
+
+// const SafeArea = styled(SafeAreaView)`
+//   flex: 1;
+//   background-color: orange;
+// `;
+
+// const MainPageHeader = styled(Text)`
+//   color: black;
+//   padding-top: ${(props) => props.theme.space[6]};
+//   padding-bottom: ${(props) => props.theme.space[6]};
+//   padding-left: ${(props) => props.theme.space[7]};
+//   font-size: ${(props) => props.theme.fontSizes.h4};
+//   font-family: ${(props) => props.theme.fonts.body};
+// `;
+
+// const MenuCard = styled(View)`
+//   height: 190px;
+//   width: 165px;
+//   elevation: 15;
+//   align-items: center;
+//   background-color: white;
+//   border-radius: 20px;
+//   padding-top: ${(props) => props.theme.space[3]};
+//   padding-horizontal: ${(props) => props.theme.space[3]};
+//   background-color: rgba(255, 255, 255, 0.7);
+// `;
+
+// const MenuCardContainer = styled(View)`
+//   flex-direction: row;
+//   justify-content: space-between;
+//   margin-top: 20px;
+//   padding-horizontal: 20px;
+// `;
+
+// const ImageCard = styled(Image)`
+//   height: 130px;
+//   border-radius: ${(props) => props.theme.space[3]};
+//   width: 100%;
+// `;
+
+// const Header = styled(Text)`
+//   font-family: ${(props) => props.theme.fonts.heading};
+//   padding-top: ${(props) => props.theme.space[4]};
+//   font-size: ${(props) => props.theme.fontSizes.body};
+//   font-weight: bold;
+//   text-align: center;
+// `;
+
+// const HeaderTwo = styled(Text)`
+//   font-family: ${(props) => props.theme.fonts.heading};
+//   padding-top: ${(props) => props.theme.space[1]};
+//   font-size: ${(props) => props.theme.fontSizes.body};
+//   font-weight: bold;
+//   text-align: center;
+// `;
+
+// export const Mainpage = ({ navigation }) => {
+//   const AdoptPage = () => {
+//     navigation.navigate("Adopt");
+//   };
+//   const PutUpAdoptionPage = () => {
+//     navigation.navigate("PutUpAdoption");
+//   };
+//   const StoriesPage = () => {
+//     navigation.navigate("ShareStories");
+//   };
+//   const FAQPage = () => {
+//     navigation.navigate("FAQ");
+//   };
+
+//     return (
+//       <SafeArea>
+//         <View>
+//           <Text variant="home">HOME</Text>
+//         </View>
+//         <MenuCardContainer>
+//           <TouchableOpacity onPress={PutUpAdoptionPage}>
+//             <MenuCard>
+//               <ImageCard source={require("../../../assets/put_up.png")} />
+//               <Text variant="title">Put Up Pet {"\n"} For Adoption</Text>
+//             </MenuCard>
+//           </TouchableOpacity>
+//           <TouchableOpacity onPress={AdoptPage}>
+//             <MenuCard>
+//               <ImageCard source={require("../../../assets/adopt.png")} />
+//               <Text variant="title">Adopt</Text>
+//             </MenuCard>
+//           </TouchableOpacity>
+//         </MenuCardContainer>
+//         <MenuCardContainer>
+//           <TouchableOpacity onPress={StoriesPage}>
+//             <MenuCard>
+//               <ImageCard source={require("../../../assets/share_stories.png")} />
+//               <Text variant="title">Share Stories</Text>
+//             </MenuCard>
+//           </TouchableOpacity>
+//           <TouchableOpacity onPress={FAQPage}>
+//             <MenuCard>
+//               <ImageCard source={require("../../../assets/faq.png")} />
+//               <Text variant="title">FAQ</Text>
+//             </MenuCard>
+//           </TouchableOpacity>
+//         </MenuCardContainer>
+//       </SafeArea>
+//     );
+//   };
+
 import React from "react";
 import {
   SafeAreaView,
+  Text,
   View,
   Dimensions,
   TouchableOpacity,
@@ -8,13 +129,37 @@ import {
 } from "react-native";
 import { Menu } from "react-native-paper";
 import styled from "styled-components/native";
-import { Text } from "../../components/typography/text.component"
 
 const { width } = Dimensions.get("screen");
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
   background-color: orange;
+`;
+
+const MainPageHeader = styled(Text)`
+  color: black;
+  padding-top: ${(props) => props.theme.space[6]};
+  padding-bottom: ${(props) => props.theme.space[6]};
+  font-size: ${(props) => props.theme.fontSizes.h4};
+  font-family: ${(props) => props.theme.fonts.heading};
+  text-align: center;
+`;
+
+const Header = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  padding-top: ${(props) => props.theme.space[4]};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  font-weight: bold;
+  text-align: center;
+`;
+
+const HeaderTwo = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  padding-top: ${(props) => props.theme.space[1]};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  font-weight: bold;
+  text-align: center;
 `;
 
 const MenuCard = styled(View)`
@@ -59,19 +204,20 @@ export const Mainpage = ({ navigation }) => {
   return (
     <SafeArea>
       <View>
-        <Text variant='home'>HOME</Text>
+        <MainPageHeader>HOME</MainPageHeader>
+        {/* <Text variant="home">HOME</Text> */}
       </View>
       <MenuCardContainer>
         <TouchableOpacity onPress={PutUpAdoptionPage}>
           <MenuCard>
             <ImageCard source={require("../../../assets/put_up.png")} />
-            <Text variant='title'>Put Up Pet {'\n'} For Adoption</Text>
+            <HeaderTwo>Put Up Pet For Adoption</HeaderTwo>
           </MenuCard>
         </TouchableOpacity>
         <TouchableOpacity onPress={AdoptPage}>
           <MenuCard>
             <ImageCard source={require("../../../assets/adopt.png")} />
-            <Text variant='title'>Adopt</Text>
+            <Header>Adopt</Header>
           </MenuCard>
         </TouchableOpacity>
       </MenuCardContainer>
@@ -79,13 +225,13 @@ export const Mainpage = ({ navigation }) => {
         <TouchableOpacity onPress={StoriesPage}>
           <MenuCard>
             <ImageCard source={require("../../../assets/share_stories.png")} />
-            <Text variant='title'>Share Stories</Text>
+            <Header>Share Stories</Header>
           </MenuCard>
         </TouchableOpacity>
         <TouchableOpacity onPress={FAQPage}>
           <MenuCard>
             <ImageCard source={require("../../../assets/faq.png")} />
-            <Text variant='title'>FAQ</Text>
+            <Header>FAQ</Header>
           </MenuCard>
         </TouchableOpacity>
       </MenuCardContainer>
--- PETrons/src/features/mainpage/put-up-for-adoption/put-up-adoption.screen.js
+++ PETrons/src/features/mainpage/put-up-for-adoption/put-up-adoption.screen.js
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import React, { useState, useCallback, useMemo, useRef } from "react";
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
 import styled from "styled-components/native";
 import { Button, TextInput } from "react-native-paper";
+import Animated from "react-native-reanimated";
+import BottomSheet from "reanimated-bottom-sheet";
+import Render from "react-native-web/dist/cjs/exports/render";
+import * as ImagePicker from "expo-image-picker";
 
 import { colors } from "../../../infrastructure/theme/colors";
 import { Spacer } from "../../../components/spacer/spacer.component";
@@ -10,12 +21,16 @@ const Container = styled.View`
   background-color: rgba(255, 255, 255, 0.7);
   padding: ${(props) => props.theme.space[4]};
   margin-top: ${(props) => props.theme.space[1]};
+  padding-top: 10px;
+  align-items: center;
+  justify-content: center;
 `;
 
 const PutUpAdoptionPageHeader = styled(Text)`
   color: black;
   font-size: ${(props) => props.theme.fontSizes.h5};
-  font-family: ${(props) => props.theme.fonts.body};
+  font-family: ${(props) => props.theme.fonts.monospace};
+  padding-top: 40px;
 `;
 
 const FormButton = styled(Button).attrs({
@@ -23,6 +38,15 @@ const FormButton = styled(Button).attrs({
 })`
   padding: ${(props) => props.theme.space[2]};
   width: 300px;
+  margin-top: 10px;
+`;
+
+const SubmitFormButton = styled(Button).attrs({
+  color: colors.button.primary,
+})`
+  padding: ${(props) => props.theme.space[2]};
+  width: 300px;
+  margin-bottom: 20px;
 `;
 
 const Background = styled.View`
@@ -44,6 +68,38 @@ const DescriptionInput = styled(TextInput)`
   textalignvertical: "top";
 `;
 
+const RenderContentContainer = styled(View)`
+  background-color: white;
+  height: 350px;
+  padding: 20px;
+`;
+
+const RenderContentTitle = styled(Text)`
+  font-size: 27px;
+  height: 35px;
+`;
+
+const RenderContentSubtitle = styled(Text)`
+  font-size: 14px;
+  color: gray;
+  height: 30px;
+  margin-bottom: 10px;
+`;
+
+const RenderContentButton = styled(TouchableOpacity)`
+  padding: 13px;
+  border-radius: 10px;
+  background-color: #ff6347;
+  align-items: center;
+  margin-vertical: 7px;
+`;
+
+const RenderContentButtonTitle = styled(Text)`
+  font-size: 17px;
+  font-weight: bold;
+  color: white;
+`;
+
 export const PutUpAdoptionPage = ({ navigation }) => {
   const [organisationType, setOrganisationType] = useState("");
   const [name, setName] = useState("");
@@ -53,104 +109,176 @@ export const PutUpAdoptionPage = ({ navigation }) => {
   const [age, setAge] = useState("");
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null);
+
+  const renderContent = () => (
+    <RenderContentContainer>
+      <View style={{ alignItems: "center" }}>
+        <RenderContentTitle>Upload Photo</RenderContentTitle>
+        <RenderContentSubtitle>Choose Your Pet Image</RenderContentSubtitle>
+      </View>
+      <RenderContentButton onPress={takePhotoFromCamera}>
+        <RenderContentButtonTitle>Take Photo</RenderContentButtonTitle>
+      </RenderContentButton>
+      <RenderContentButton onPress={chooseFromLibrary}>
+        <RenderContentButtonTitle>Choose From Library</RenderContentButtonTitle>
+      </RenderContentButton>
+      <RenderContentButton onPress={() => sheetRef.current.snapTo(2)}>
+        <RenderContentButtonTitle>Cancel</RenderContentButtonTitle>
+      </RenderContentButton>
+    </RenderContentContainer>
+  );
+
+  const sheetRef = React.useRef(null);
+
+  const chooseFromLibrary = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+      sheetRef.current.snapTo(2);
+    }
+  };
+
+  const takePhotoFromCamera = async () => {
+    // Ask the user for the permission to access the camera
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("You've refused to allow this appp to access your camera!");
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync();
+
+    // Explore the result
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+      sheetRef.current.snapTo(2);
+    }
+  };
 
   return (
     <Background>
       <PutUpAdoptionPageHeader>
         Fill in your pet details:
       </PutUpAdoptionPageHeader>
-      <Container>
-        <FormButton
-          icon="image"
-          mode="contained"
-          onPress={() => navigation.goBack()}
-        >
-          Upload Image
-        </FormButton>
-        <Spacer size="large">
-          <Inputs
-            label="Organisation"
-            value={organisationType}
-            textContentType="none"
-            keyboardType="default"
-            autoCapitalize="none"
-            onChangeText={(text) => setOrganisationType(text)}
-          />
-        </Spacer>
-        <Spacer size="large">
-          <Inputs
-            label="Pet Name"
-            value={name}
-            textContentType="name"
-            keyboardType="default"
-            autoCapitalize="none"
-            onChangeText={(text) => setName(text)}
-          />
-        </Spacer>
-        <Spacer size="large">
-          <Inputs
-            label="Animal Type"
-            value={type}
-            textContentType="none"
-            keyboardType="default"
-            autoCapitalize="none"
-            onChangeText={(text) => setType(text)}
-          />
-        </Spacer>
-        <Spacer size="large">
-          <Inputs
-            label="Animal Breed"
-            value={breed}
-            textContentType="none"
-            keyboardType="default"
-            autoCapitalize="none"
-            onChangeText={(text) => setBreed(text)}
-          />
-        </Spacer>
-        <Spacer size="large">
-          <Inputs
-            label="Pet Gender"
-            value={gender}
-            textContentType="none"
-            keyboardType="default"
-            autoCapitalize="none"
-            onChangeText={(text) => setGender(text)}
-          />
-        </Spacer>
-        <Spacer size="large">
-          <Inputs
-            label="Age"
-            value={age}
-            textContentType="none"
-            keyboardType="number-pad"
-            onChangeText={(text) => setAge(text)}
-          />
-        </Spacer>
-        <Spacer size="large">
-          <Inputs
-            label="Price"
-            value={price}
-            textContentType="none"
-            keyboardType="number-pad"
-            onChangeText={(text) => setPrice(text)}
-          />
-        </Spacer>
-        <Spacer size="large">
-          <DescriptionInput
-            label="Short Description..."
-            value={description}
-            textContentType="none"
-            autoCapitalize="none"
-            keyboardType="default"
-            multiline={true}
-            onChangeText={(text) => setDescription(text)}
-          />
-        </Spacer>
-      </Container>
+      <BottomSheet
+        initialSnap={2}
+        ref={sheetRef}
+        snapPoints={[450, 300, 0]}
+        borderRadius={10}
+        renderContent={renderContent}
+      />
+      <ScrollView>
+        <Container>
+          {image && (
+            <Image
+              source={{ uri: image }}
+              style={{ width: 200, height: 200 }}
+            />
+          )}
+          <FormButton
+            icon="image"
+            mode="contained"
+            onPress={() => sheetRef.current.snapTo(0)}
+          >
+            Upload Image
+          </FormButton>
+          <Spacer size="large">
+            <Inputs
+              label="Organisation"
+              value={organisationType}
+              textContentType="none"
+              keyboardType="default"
+              autoCapitalize="none"
+              onChangeText={(text) => setOrganisationType(text)}
+            />
+          </Spacer>
+          <Spacer size="large">
+            <Inputs
+              label="Pet Name"
+              value={name}
+              textContentType="name"
+              keyboardType="default"
+              autoCapitalize="none"
+              onChangeText={(text) => setName(text)}
+            />
+          </Spacer>
+          <Spacer size="large">
+            <Inputs
+              label="Animal Type"
+              value={type}
+              textContentType="none"
+              keyboardType="default"
+              autoCapitalize="none"
+              onChangeText={(text) => setType(text)}
+            />
+          </Spacer>
+          <Spacer size="large">
+            <Inputs
+              label="Animal Breed"
+              value={breed}
+              textContentType="none"
+              keyboardType="default"
+              autoCapitalize="none"
+              onChangeText={(text) => setBreed(text)}
+            />
+          </Spacer>
+          <Spacer size="large">
+            <Inputs
+              label="Pet Gender"
+              value={gender}
+              textContentType="none"
+              keyboardType="default"
+              autoCapitalize="none"
+              onChangeText={(text) => setGender(text)}
+            />
+          </Spacer>
+          <Spacer size="large">
+            <Inputs
+              label="Age"
+              value={age}
+              textContentType="none"
+              keyboardType="number-pad"
+              onChangeText={(text) => setAge(text)}
+            />
+          </Spacer>
+          <Spacer size="large">
+            <Inputs
+              label="Price"
+              value={price}
+              textContentType="none"
+              keyboardType="number-pad"
+              onChangeText={(text) => setPrice(text)}
+            />
+          </Spacer>
+          <Spacer size="large">
+            <DescriptionInput
+              label="Short Description..."
+              value={description}
+              textContentType="none"
+              autoCapitalize="none"
+              keyboardType="default"
+              multiline={true}
+              onChangeText={(text) => setDescription(text)}
+            />
+          </Spacer>
+        </Container>
+      </ScrollView>
       <Spacer size="large">
-        <FormButton mode="contained" onPress={() => navigation.goBack()}>
+        <SubmitFormButton mode="contained" onPress={() => navigation.goBack()}>
           Confirm
-        </FormButton>
+        </SubmitFormButton>
       </Spacer>
     </Background>
   );
