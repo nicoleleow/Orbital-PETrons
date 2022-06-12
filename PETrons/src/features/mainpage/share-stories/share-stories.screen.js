9efcbe79983a2e2be09937fc110cdff4a0acc0f5
tungkei <e0774490@u.nus.edu>
2022-06-12 20:31:52 +0800

    get rid of imports, try to debug error on ios

--- PETrons/src/features/mainpage/share-stories/share-stories.screen.js
+++ PETrons/src/features/mainpage/share-stories/share-stories.screen.js
@@ -3,8 +3,6 @@ import { SafeAreaView, View, ScrollView } from "react-native";
 import styled from "styled-components/native";
 import { Text } from "../../../components/typography/text.component"
 
-import storage from '@react-native-firebase/storage';
-
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
   background-color: orange;
