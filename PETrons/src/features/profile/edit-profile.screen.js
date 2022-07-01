cbe5428c19468e806a1b246e63cb7c1e1ce25fc1
nicoleleow <e0774639@u.nus.edu>
2022-07-01 21:24:21 +0800

    implemented edit profile feature

--- PETrons/src/features/profile/edit-profile.screen.js
+++ PETrons/src/features/profile/edit-profile.screen.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { SafeAreaView, View } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  StyleSheet,
+  TextInput,
+  Pressable,
+} from "react-native";
+// import { TextInput } from "react-native-paper";
 import styled from "styled-components/native";
 import { Text } from "../../components/typography/text.component";
 
@@ -8,10 +17,77 @@ const SafeArea = styled(SafeAreaView)`
   background-color: orange;
 `;
 
+const PageHeader = styled(Text)`
+  color: black;
+  font-size: ${(props) => props.theme.fontSizes.h5};
+  font-family: ${(props) => props.theme.fonts.monospace};
+  margin-top: ${(props) => props.theme.space[5]};
+  text-align: center;
+  margin-bottom: ${(props) => props.theme.space[5]};
+`;
+
+const ProfilePicture = styled(View)`
+  height: 100px;
+  width: 100px;
+  border-radius: 15px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImageText = styled(Text)`
+  color: gainsboro;
+  font-size: ${(props) => props.theme.fontSizes.title};
+  font-family: ${(props) => props.theme.fonts.monospace};
+  margin-top: ${(props) => props.theme.space[2]};
+  margin-bottom: ${(props) => props.theme.space[2]};
+  text-align: center;
+  text-decoration-line: underline;
+`;
+
+const FieldInput = styled(TextInput)`
+  flex-direction: row;
+  margin-top: ${(props) => props.theme.space[5]};
+  margin-bottom: ${(props) => props.theme.space[3]};
+  border-bottom-width: 1px;
+  border-bottom-color: whitesmoke;
+  padding-bottom: 5px;
+  width: 340px;
+`;
+
 export const EditProfilePage = () => (
   <SafeArea>
     <View>
-      <Text variant="header">Edit</Text>
+      <PageHeader>Edit Your Profile:</PageHeader>
+    </View>
+    <View style={{ alignItems: "center" }}>
+      <TouchableOpacity>
+        <ProfilePicture>
+          <ImageBackground
+            source={require("../../../assets/postIt_icon.png")}
+            style={{ height: 100, width: 100 }}
+            imageStyle={{ borderRadius: 15 }}
+            backgroundColor="black"
+          ></ImageBackground>
+        </ProfilePicture>
+      </TouchableOpacity>
+      <Pressable onPress={() => console.log("hi")}>
+        <ImageText>Change profile photo</ImageText>
+      </Pressable>
+      <FieldInput
+        placeholder="Email"
+        placeholderTextColor="black"
+        autoCorrect={false}
+      ></FieldInput>
+      <FieldInput
+        placeholder="Username"
+        placeholderTextColor="black"
+        autoCorrect={false}
+      ></FieldInput>
+      <FieldInput
+        placeholder="Password"
+        placeholderTextColor="black"
+        autoCorrect={false}
+      ></FieldInput>
     </View>
   </SafeArea>
 );
